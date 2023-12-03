fccb5bb67f0834c8446638348a4d713ca8473236
cbendernagel <cbendernagel@gmail.com>
2023-12-03 18:43:53 -0500

    changing to single iteration

--- 1/solution.java
+++ 1/solution.java
@@ -8,32 +8,31 @@ class Solution {
         System.out.println("test");
 
         try {
-            File input = new File("input.txt");
+            File input = new File("1/input.txt");
             Scanner reader = new Scanner(input);
             long sum = 0;
             int lineNum = 0;
             while(reader.hasNextLine()) {
+                int first = -1, last = -1;
                 char[] line = reader.nextLine().toCharArray();
                 // find the first left number
                 for(int i = 0; i < line.length; i++) {
                     if(Character.isDigit(line[i])) {
-                        lineNum = ((line[i] - 48) * 10);
-                        // sum = sum + lineNum;
-                        break;
+                        if(first == -1) {
+                            first = line[i] - 48;
+                        }
+
+                        last = line[i] - 48;
                     }
                 }
 
-                // find the first number from right
-                for(int j = line.length - 1; j >= 0; j--){ 
-                    if(Character.isDigit(line[j])) {
-                        lineNum += (line[j] - 48);
-                        sum = sum + lineNum;
-                        break;
-                    }
-                }
+                lineNum = ((first * 10) + last);
+                sum += lineNum;
                 
                 System.out.println(lineNum + " - " + sum);
             }
+
+            reader.close();
         }
         catch (FileNotFoundException e) {
             System.out.println("file does not exist");
